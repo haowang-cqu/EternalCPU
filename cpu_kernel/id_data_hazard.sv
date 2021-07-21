c59ae2db0f4ecbfc0e4769bdefc1d4a0445f6eb8
FanQinFred <1836459118@qq.com>
2021-07-21 12:50:40 +0800

    Update id_data_hazard.sv

--- cpu_kernel/id_data_hazard.sv
+++ cpu_kernel/id_data_hazard.sv
@@ -38,8 +38,10 @@ module id_reg_harzrd (
     // 3、判断当前回写阶段的写入目标，是否是当前寄存器，如果是就将回写阶段的结果当作读取结果
     // 4、都不是的情况下，就将读取结果作为最终结果
 
-    assign branch_stall_o = (((branch_flag_i==1 || jr_flag_hazard_i==1) && ex_we_i == 1'b1 && ex_waddr_i == reg_addr1_i && reg_addr1_i!=0  ) ? 1  : 0 ) | 
-                          (((branch_flag_i==1 || jr_flag_hazard_i==1) && ex_we_i == 1'b1 && ex_waddr_i == reg_addr2_i  &&  reg_addr2_i!=0) ? 1  : 0 );
+    // assign branch_stall_o = (((branch_flag_i==1 || jr_flag_hazard_i==1) && ex_we_i == 1'b1 && ex_waddr_i == reg_addr1_i && reg_addr1_i!=0  ) ? 1  : 0 ) | 
+    //                       (((branch_flag_i==1 || jr_flag_hazard_i==1) && ex_we_i == 1'b1 && ex_waddr_i == reg_addr2_i  &&  reg_addr2_i!=0) ? 1  : 0 );
+    assign branch_stall_o = ((ex_we_i == 1'b1 && ex_waddr_i == reg_addr1_i && reg_addr1_i!=0  ) ? 1  : 0 ) | 
+                            ((ex_we_i == 1'b1 && ex_waddr_i == reg_addr2_i  &&  reg_addr2_i!=0) ? 1  : 0 );
 
     assign rdata1_o =   (rst_i == 1'b1                                  ) ? `ZeroWord   :
                         (reg_addr1_i == 0                               ) ? reg_data1_i :
